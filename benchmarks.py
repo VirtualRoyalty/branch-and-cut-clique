4d95a7e3eb75af928346836d0c0ca54d17d94a3e
Vadim Alperovich <vadikalperovich@mail.ru>
2021-11-21 14:38:06 +0300

    added dict for tests on benchmarks

---
+++ benchmarks.py
@@ -0,0 +1,61 @@
+EASY = {
+    'benchmarks/DIMACS_all_ascii/johnson8-2-4.clq': 4,
+    'benchmarks/DIMACS_all_ascii/johnson16-2-4.clq': 8,
+    'benchmarks/DIMACS_all_ascii/MANN_a9.clq': 16,
+    # 'benchmarks/DIMACS_all_ascii/gen200_p0.9_55.clq': 55,
+    'benchmarks/DIMACS_all_ascii/keller4.clq': 11,
+    'benchmarks/DIMACS_all_ascii/c-fat200-1.clq': None,
+    'benchmarks/DIMACS_all_ascii/c-fat500-1.clq': None,
+    'benchmarks/DIMACS_all_ascii/c-fat500-10.clq': None,
+    'benchmarks/DIMACS_all_ascii/c-fat200-2.clq': None,
+    'benchmarks/DIMACS_all_ascii/hamming8-4.clq': 16,
+}
+
+MEDIUM = {
+    'benchmarks/DIMACS_all_ascii/gen200_p0.9_55.clq': 55,
+    # 'benchmarks/DIMACS_all_ascii/gen200_p0.9_44.clq': 44,
+    'benchmarks/DIMACS_all_ascii/C125.9.clq': 34,
+    'benchmarks/DIMACS_all_ascii/brock200_2.clq': 12,
+    'benchmarks/DIMACS_all_ascii/brock200_3.clq': 15,
+    'benchmarks/DIMACS_all_ascii/brock200_4.clq': 17,
+    'benchmarks/DIMACS_all_ascii/brock200_1.clq': 21,
+    'benchmarks/DIMACS_all_ascii/p_hat300-1.clq': 8,
+    'benchmarks/DIMACS_all_ascii/p_hat1000-1.clq': 10,
+    'benchmarks/DIMACS_all_ascii/san1000.clq': 15,
+}
+
+HARD = {
+    'benchmarks/DIMACS_all_ascii/brock400_1.clq': 27,
+    'benchmarks/DIMACS_all_ascii/brock400_2.clq': 29,
+    'benchmarks/DIMACS_all_ascii/brock400_3.clq': 31,
+    'benchmarks/DIMACS_all_ascii/brock400_4.clq': 33,
+    'benchmarks/DIMACS_all_ascii/MANN_a27.clq': 126,
+    'benchmarks/DIMACS_all_ascii/MANN_a45.clq': 345,
+    'benchmarks/DIMACS_all_ascii/sanr400_0.7.clq': 21,
+    'benchmarks/DIMACS_all_ascii/sanr400_0.9.clq': 42,
+    'benchmarks/DIMACS_all_ascii/p_hat1000-2.clq': 46,
+    'benchmarks/DIMACS_all_ascii/p_hat500-3.clq': 50,
+    'benchmarks/DIMACS_all_ascii/p_hat1500-1.clq': 12,
+    'benchmarks/DIMACS_all_ascii/p_hat300-3.clq': 36,
+}
+
+REST = {
+    # 'benchmarks/DIMACS_all_ascii/san200_0.7_1.clq': 30,
+    # 'benchmarks/DIMACS_all_ascii/san200_0.7_2.clq': 18,
+    # 'benchmarks/DIMACS_all_ascii/san200_0.9_1.clq': 70,
+    # 'benchmarks/DIMACS_all_ascii/san200_0.9_2.clq': 60,
+    # 'benchmarks/DIMACS_all_ascii/san200_0.9_3.clq': 44,
+    # 'benchmarks/DIMACS_all_ascii/sanr200_0.7.clq': 18,
+    # 'benchmarks/DIMACS_all_ascii/c-fat200-5.clq': 58,
+    # 'benchmarks/DIMACS_all_ascii/c-fat500-2.clq': 26,
+    # 'benchmarks/DIMACS_all_ascii/c-fat500-5.clq': 64,
+    # 'benchmarks/DIMACS_all_ascii/johnson8-4-4.clq': 14,
+    # 'benchmarks/DIMACS_all_ascii/hamming6-2.clq': 32,
+    # 'benchmarks/DIMACS_all_ascii/hamming6-4.clq': 4,
+    # 'benchmarks/DIMACS_all_ascii/hamming8-2.clq': 128,
+    'benchmarks/DIMACS_all_ascii/MANN_a27.clq': 126,
+    'benchmarks/DIMACS_all_ascii/MANN_a45.clq': 345,
+    # 'benchmarks/DIMACS_all_ascii/p_hat300-2.clq': 25,
+    # 'benchmarks/DIMACS_all_ascii/p_hat300-3.clq': 36,
+
+}
