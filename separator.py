c5b3e8ada0d1e51a2b7e6df7f782b43a434becb0
Vadim Alperovich <vadikalperovich@mail.ru>
2021-11-21 23:52:45 +0300

    simple sep moved to ILS

--- separator.py
+++ separator.py
@@ -7,26 +7,8 @@ from tqdm import tqdm
 import utils
 
 
-def simple_separation(graph: nx.Graph, solution: list,
-                      first_k: int = 10, abs_tol=1e-3, **kwargs) -> list:
-    coloring_dct = nx.coloring.greedy_color(graph, strategy=nx.coloring.strategy_random_sequential)
-    independent_sets = list()
-    for index in range(1, first_k):
-        random_index = np.random.randint(0, len(solution))
-        ind_set = set()
-        set_weight = 0
-        for _node, color in coloring_dct.items():
-            if color == coloring_dct[random_index]:
-                ind_set.add(_node)
-                set_weight += solution[_node]
-        if len(ind_set) > 2:  # and set_weight > (1 + abs_tol):
-            independent_sets.append((ind_set, set_weight))
-    independent_sets = sorted(independent_sets, key=lambda item: (item[1], len(item[0])), reverse=True)
-    return independent_sets[:first_k]  # random.sample(independent_sets, k=min(len(independent_sets), first_k))
-
-
 def greedy_separation(graph: nx.Graph, solution: list,
-                      n_iter: int = 20, first_k: int = 10, abs_tol=1e-3) -> list:
+                      n_iter: int = 1, first_k: int = 10, abs_tol=1e-3) -> list:
     solution_indexes = np.argsort(solution)
     independent_sets = list()
     for _ in range(n_iter):
@@ -119,6 +101,25 @@ class IteratedLocalSearch:
         self.searcher = LocalSearch(graph, weights)
         return
 
+    @staticmethod
+    def get_ind_set(graph: nx.Graph, solution: list,
+                    first_k: int = 10, abs_tol=1e-3, **kwargs) -> list:
+        coloring_dct = nx.coloring.greedy_color(graph, strategy=nx.coloring.strategy_random_sequential)
+        n_colors = len(set(coloring_dct.values()))
+        independent_sets = list()
+        for index in range(1, first_k):
+            random_color = np.random.randint(0, n_colors)
+            ind_set = set()
+            set_weight = 0
+            for _node, color in coloring_dct.items():
+                if color == random_color:  # coloring_dct[random_index]:
+                    ind_set.add(_node)
+                    set_weight += solution[_node]
+            if len(ind_set) > 2:  # and set_weight > (1 + abs_tol):
+                independent_sets.append((ind_set, set_weight))
+        independent_sets = sorted(independent_sets, key=lambda item: (item[1], len(item[0])), reverse=True)
+        return independent_sets[:first_k]
+
     def reset(self):
         self.best_sol_set = self.init_sol_set
         self.best_others_set = self.init_others_set
@@ -169,58 +170,6 @@ class IteratedLocalSearch:
         return
 
 
-class FastLocalSearch:
-    def __init__(self, graph: nx.Graph, solution: np.array, weights: list):
-        self.graph = graph
-        self.num_of_nodes = self.graph.number_of_nodes()
-        self.solution = solution
-        self.weights = weights
-        self.tightness = np.array(weights, dtype=np.float)
-        self.qco = np.zeros(self.num_of_nodes, dtype=np.int)
-        self.index_qco = np.array(range(0, self.num_of_nodes), dtype=np.int)
-        self.q = 0
-        self.c = self.num_of_nodes
-        self.init_to_qco()
-        return
-
-    def swap_qco(self, i, l):
-        j = self.qco[l]
-        self.qco[self.index_qco[i]], self.qco[self.qco[l]] \
-            = self.qco[self.qco[l]], self.qco[self.index_qco[i]]
-        self.index_qco[i], self.index_qco[j] = self.index_qco[j], self.index_qco[i]
-        return
-
-    def init_qco(self):
-        for node_index, in_solution in enumerate(self.solution):
-            if not in_solution:
-                continue
-        self.insert_to_qco(node_index)
-        return
-
-    def insert_to_qco(self, node_index):
-        self.swap_qco(node_index, self.q)
-        self.q += 1
-        self.c -= 1
-        for neigh in self.graph.neighbors(node_index + 1):
-            if self.tightness[neigh - 1] > 0:
-                self.swap_qco(neigh - 1, self.c)
-                self.c -= 1
-                self.tightness[neigh - 1] -= self.weights[node_index]
-        return
-
-    def run(self, n_iter=10):
-        for i in n_iter:
-            qco_i = np.random.randint(self.q + 1, self.q + self.c + 1)
-            node_i = self.qco[qco_i]
-            if self.solution[node_i] == 0 and self.mu[node_i] > 0:
-                self.solution[node_i] == 0
-                for neigh in self.graph.neighbors(node_i + 1):
-                    if self.solution[neigh - 1] == 1:
-                        self.solution[neigh - 1] = 0
-                        self.mu[neigh - 1] -= self.weights[node_i]
-        return
-
-
 if __name__ == '__main__':
     from utils import read_graph_file
 
@@ -232,14 +181,14 @@ if __name__ == '__main__':
     ph.design_problem(accepted_sets_ratio=0.15)
     ph.solve_problem()
     solution = ph.get_solution()
-    ind_sets = simple_separation(ph.graph, solution, n_iter=1, first_k=20)
+    ind_sets = IteratedLocalSearch.get_ind_set(ph.graph, solution, n_iter=1, first_k=15)
     print('Simple separator:', *ind_sets[:10], sep='\n')
     sub = G.subgraph(ind_sets[0][0])
     print('\tCheck:', True if sub.number_of_edges() == 0 else False, '\n')
 
     ils = IteratedLocalSearch(G, ind_sets[0][0], weights=solution)
     for i in range(5):
-        exec_time = ils.timed_run(n_iter=200, verbose=False)
+        exec_time = ils.timed_run(n_iter=100, verbose=False)
         _minutes, _seconds = divmod(exec_time, 60)
         print('Solution weights:', sum([solution[x] for x in ils.best_sol_set]))
         print('RESULT', ils.best_sol_set, 'SET WEIGHT', ils.best_set_weight)
